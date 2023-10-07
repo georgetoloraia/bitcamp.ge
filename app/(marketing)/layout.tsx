786a3c88c8388dbb23cb01232efaca237399f4e5
otarza <otar.zakalashvili@gmail.com>
2023-10-08 01:47:44 +0400

    Update ssh agent config.

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -26,7 +26,7 @@ export default async function MarketingLayout({
                 "px-4"
               )}
             >
-              რეგისტრაცია
+              რეგისტრაცია 🔥
             </Link>
           </nav>
         </div>
