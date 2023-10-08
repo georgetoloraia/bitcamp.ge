9d9c5e47723349daea23adbf22ef55e10383fdef
otarza <otar.zakalashvili@gmail.com>
2023-10-08 04:40:04 +0400

    Test build.

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -26,7 +26,7 @@ export default async function MarketingLayout({
                 "px-4"
               )}
             >
-              რეგისტრაცია 🔥 🔥 🔥
+              რეგისტრაცია 🔥
             </Link>
           </nav>
         </div>
