116b3c40c4d7029d000929ffbb773a0b1f822252
otarza <otar.zakalashvili@gmail.com>
2023-10-08 04:15:02 +0400

    Add simple deploy test.

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -26,7 +26,7 @@ export default async function MarketingLayout({
                 "px-4"
               )}
             >
-              რეგისტრაცია 🔥 🔥
+              რეგისტრაცია 🔥 🔥 🔥
             </Link>
           </nav>
         </div>
