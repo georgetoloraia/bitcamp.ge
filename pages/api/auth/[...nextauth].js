65f60d17de9844656e5a20c2c7a06ded66941591
NickKipshidze <kipshidze.nick@gmail.com>
2023-12-16 13:23:04 +0400

    Update next-auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,40 +1,49 @@
 import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
+import CredentialsProvider from "next-auth/providers/credentials"
 
 export default NextAuth({
   providers: [
-    Providers.Credentials({
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        username: { label: "username", type: "text" },
+        email: { label: "email", type: "email" },
+        phone_number: { label: "phone_number", type: "text" },
+        password: { label: "password", type: "password" },
+      },
       async authorize(credentials) {
-        const res = await fetch(`${process.env.BACKEND_API_URL}/auth/login/`, {
+        const res = await fetch(`${process.env.BACKEND_API_URL}/auth/signup`, {
           method: "POST",
           body: JSON.stringify(credentials),
-          headers: { "Content-Type": "application/json" }
+          headers: { "Content-Type": "application/json" },
         })
+
         const user = await res.json()
-        
+
         if (res.ok && user) {
           return user
         }
+
         return null
-      }
-    })
+      },
+    }),
   ],
   session: {
     jwt: true,
   },
   pages: {
-    signIn: "/",
+    signIn: "login",
   },
   callbacks: {
     async jwt(token, user) {
       if (user) {
-        token.id = user.id;
+        token.user.token = user.id
       }
-      return token;
+      return token
     },
     async session(session, token) {
-      session.user.id = token.id;
-      return session;
+      session.user.id = token.id
+      return session
     },
   },
 })
