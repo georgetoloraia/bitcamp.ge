15023efe4597b271ad2c8c16c2ef2c9c79417f99
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2023-12-18 12:48:54 +0400

    add chapter 13

--- config/docs.ts
+++ config/docs.ts
@@ -303,6 +303,10 @@ export const docsConfig: DocsConfig = {
           title: "თავი 12",
           href: "/classes/python/2023f/week-12",
         },
+        {
+          title: "თავი 13",
+          href: "/classes/python/2023f/week-13",
+        },
       ],
     },
   ],
