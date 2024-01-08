b12cd9b491861a6b4e0ac1dcdce302cf40b74122
otarza <otar.zakalashvili@gmail.com>
2024-01-08 08:47:57 +0400

    Fix session and jwt callbacks to get proper user data.

--- lib/auth.ts
+++ lib/auth.ts
@@ -14,61 +14,13 @@ export const authOptions: NextAuthOptions = {
   // huh any! I know.
   // This is a temporary fix for prisma client.
   // @see https://github.com/prisma/prisma/issues/16117
-  adapter: PrismaAdapter(db as any),
   session: {
     strategy: "jwt",
   },
   pages: {
     signIn: "/login",
   },
-  providers: [
-    GitHubProvider({
-      clientId: env.GH_CLIENT_ID,
-      clientSecret: env.GH_CLIENT_SECRET,
-    }),
-    EmailProvider({
-      from: env.SMTP_FROM,
-      sendVerificationRequest: async ({ identifier, url, provider }) => {
-        const user = await db.user.findUnique({
-          where: {
-            email: identifier,
-          },
-          select: {
-            emailVerified: true,
-          },
-        })
-
-        const templateId = user?.emailVerified
-          ? env.POSTMARK_SIGN_IN_TEMPLATE
-          : env.POSTMARK_ACTIVATION_TEMPLATE
-        if (!templateId) {
-          throw new Error("Missing template id")
-        }
-
-        const result = await postmarkClient.sendEmailWithTemplate({
-          TemplateId: parseInt(templateId),
-          To: identifier,
-          From: provider.from as string,
-          TemplateModel: {
-            action_url: url,
-            product_name: siteConfig.name,
-          },
-          Headers: [
-            {
-              // Set this to prevent Gmail from threading emails.
-              // See https://stackoverflow.com/questions/23434110/force-emails-not-to-be-grouped-into-conversations/25435722.
-              Name: "X-Entity-Ref-ID",
-              Value: new Date().getTime() + "",
-            },
-          ],
-        })
-
-        if (result.ErrorCode) {
-          throw new Error(result.Message)
-        }
-      },
-    }),
-  ],
+  providers: [],
   callbacks: {
     async session({ token, session }) {
       if (token) {
@@ -81,13 +33,13 @@ export const authOptions: NextAuthOptions = {
       return session
     },
     async jwt({ token, user }) {
-      const dbUser = await db.user.findFirst({
-        where: {
-          email: token.email,
+      const fetchedUser = await (await fetch("http://localhost/auth/profile", {
+        headers: {
+          Authorization: `Token ${(token.user as any).token}`, // Type assertion to 'any'
         },
-      })
+      })).json();
 
-      if (!dbUser) {
+      if (!fetchedUser) {
         if (user) {
           token.id = user?.id
         }
@@ -95,10 +47,10 @@ export const authOptions: NextAuthOptions = {
       }
 
       return {
-        id: dbUser.id,
-        name: dbUser.name,
-        email: dbUser.email,
-        picture: dbUser.image,
+        id: fetchedUser.id,
+        name: fetchedUser.username,
+        email: fetchedUser.email,
+        picture: fetchedUser.image,
       }
     },
   },
