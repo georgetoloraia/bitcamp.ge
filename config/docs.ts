e31831cbaa6e7babee2cdd271d68f4b8cc221104
Temuri Tsutskiridze <temuri.tsutskiridze.04@gmail.com>
2024-02-27 04:24:18 +0400

    add theory lecture 6

--- config/docs.ts
+++ config/docs.ts
@@ -291,6 +291,10 @@ export const docsConfig: DocsConfig = {
           title: "თავი 5",
           href: "/classes/javascript/2024w/week-5",
         },
+        {
+          title: "თავი 6",
+          href: "/classes/javascript/2024w/week-6",
+        },
       ],
     },
   ],
